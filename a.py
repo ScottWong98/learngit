d0b8014df710e8a9111d9f53bc934c36eaf8d04f
ScottWong <jmhgchn@gmail.com>
2022-01-08 16:01:52 +0800

    black a.py locally

--- a.py
+++ a.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -9,6 +8,7 @@ from torch.utils.data.dataset import TensorDataset
 import os
 from typing import Dict, List, Tuple
 
+
 class ImageDataset:
     def __init__(self, dir_name: str, batch_size: int = 16) -> None:
         super().__init__()
@@ -18,8 +18,10 @@ class ImageDataset:
     def _load_face_data(self) -> Dict[str, List[str]]:
         face_filename_dict: Dict[str, List[str]] = {}
         for sub_dir in os.listdir(self.dir_name):
-            face_filename_dict[sub_dir] = [os.path.join(self.dir_name, sub_dir, filename)
-                for filename in os.listdir(os.path.join(self.dir_name, sub_dir))]
+            face_filename_dict[sub_dir] = [
+                os.path.join(self.dir_name, sub_dir, filename)
+                for filename in os.listdir(os.path.join(self.dir_name, sub_dir))
+            ]
         return face_filename_dict
 
     def _load_emotion_data(self) -> Dict[str, List[str]]:
@@ -53,7 +55,9 @@ class ImageDataset:
                 tmp_labels.append(cnt)
             cnt += 1
             tmp_images, tmp_labels = np.array(tmp_images), np.array(tmp_labels)
-            train_ids, test_ids = train_test_split(range(len(tmp_images)), test_size=test_size, random_state=12)
+            train_ids, test_ids = train_test_split(
+                range(len(tmp_images)), test_size=test_size, random_state=12
+            )
             train_images += tmp_images[train_ids].tolist()
             train_labels += tmp_labels[train_ids].tolist()
             test_images += tmp_images[test_ids].tolist()
@@ -61,7 +65,9 @@ class ImageDataset:
 
         return self._to_dataloader(train_images, train_labels, test_images, test_labels)
 
-    def _to_dataloader(self, train_images, train_labels, test_images, test_labels) -> Tuple[DataLoader, DataLoader]:
+    def _to_dataloader(
+        self, train_images, train_labels, test_images, test_labels
+    ) -> Tuple[DataLoader, DataLoader]:
         return (
             DataLoader(
                 TensorDataset(Tensor(train_images), LongTensor(train_labels)),
